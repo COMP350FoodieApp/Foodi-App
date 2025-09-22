7aae1833e4d6a0d422d98682d1e6dd4732f176ef
Francisco Campa <francampa913@gmail.com>
2025-09-22 12:53:06 -0700

    Update

---
+++ Foodi/Foodi/FoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiApp.swift
+//  Foodi
+//
+//  Created by Francisco Campa on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Foodi/FoodiTests/FoodiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiTests.swift
+//  FoodiTests
+//
+//  Created by Francisco Campa on 9/22/25.
+//
+
+import Testing
+@testable import Foodi
+
+struct FoodiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
